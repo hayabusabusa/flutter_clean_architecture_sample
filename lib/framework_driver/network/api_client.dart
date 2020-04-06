b4f3991a75beae57e6ca1b9e212fb3a96d0a982e
hayabusabusa <hayabusabusaSY@gmail.com>
2020-04-06 14:35:25 +0900

    Add api client

---
+++ lib/framework_driver/network/api_client.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import 'package:clean_architecture_sample/secret.dart';
+// NOTE: Framework and Driver から Entity への依存は順方向
+import 'package:clean_architecture_sample/entity/entity.dart';
+
+class APIClient {
+  static const String baseURL = 'https://qiita.com/api/v2';
+  static const String accessToken = 'Bearer ' + qiitaAccessToken;
+
+  final http.Client httpClient = http.Client();
+
+  Future<List<QiitaItem>> fetchItems() async {
+    final url = baseURL + '/items';
+    final headers = {
+      "Content-Type": "application/json",
+      "Authorization": accessToken,
+    };
+    final response = await httpClient.get(url, headers: headers);
+
+    if (response.statusCode == 200) {
+      final List<dynamic> array = json.decode(response.body);
+      return array.map((i) => QiitaItem.fromJson(i)).toList();
+    } else {
+      throw Exception('Failed to GET /items. (code=${response.statusCode})');
+    }
+  }
+}
