30b0bcac380d23a88e50d338dd519c87def91b69
hayabusabusa <hayabusabusaSY@gmail.com>
2020-04-09 19:20:22 +0900

    Add custom page route

--- lib/framework_driver/ui/widget/widgets.dart
+++ lib/framework_driver/ui/widget/widgets.dart
@@ -1,5 +1,6 @@
+export './article_item.dart';
+export './fade_route.dart';
 export './indicator.dart';
 export './likes_count.dart';
 export './ripple_card.dart';
 export './user_icon.dart';
-export './article_item.dart';
