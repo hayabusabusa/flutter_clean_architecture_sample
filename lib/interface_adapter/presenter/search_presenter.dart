35b1e56b891db78da0278864b4bd1d5a49089290
hayabusabusa <hayabusabusaSY@gmail.com>
2020-04-10 09:49:55 +0900

    Add search presenter

---
+++ lib/interface_adapter/presenter/search_presenter.dart
@@ -0,0 +1,59 @@
+import 'package:clean_architecture_sample/entity/entity.dart';
+import 'package:clean_architecture_sample/usecase/usecase.dart';
+
+/// `View` 側に公開する `SearchPresenter` のInputインターフェース
+abstract class SearchPresenterInput {
+  void onTextEditedAndExit(String text);
+  void onTapListItem(QiitaItem item);
+  void onReachBottom();
+}
+
+/// `View` 側に実装させる `SearchPresenter` のOutputインターフェース
+abstract class SearchPresenterOutput {
+  Function(List<QiitaItem>) updateArticles;
+  Function(bool) updateIsLoading;
+  Function(String) recieveError;
+}
+
+class SearchPresenter implements SearchPresenterInput, SearchUseCaseOutput {
+  SearchUseCaseInput _useCase;
+
+  SearchPresenterOutput output;
+
+  SearchPresenter(
+    SearchUseCaseInput useCase,
+  ): assert(useCase != null),
+     this._useCase = useCase;
+
+  @override
+  void onTextEditedAndExit(String text) {
+    _useCase.fetchArticles(text);
+  }
+
+  @override
+  void onTapListItem(QiitaItem item) {
+    _useCase.openURL(item.url);
+  }
+
+  @override
+  void onReachBottom() {
+    _useCase.fetchNextPageArticles();
+  }
+
+  // MARK: UseCase Output
+
+  @override
+  void useCaseDidUpdateArticles(List<QiitaItem> articles) {
+    output.updateArticles(articles);
+  }
+
+  @override
+  void useCaseIsLoading(bool isLoading) {
+    output.updateIsLoading(isLoading);
+  }
+
+  @override
+  void useCaseDidRecieveError(Exception error) {
+    output.recieveError(error.toString());
+  }
+}
